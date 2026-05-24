aad8aec7e505df10cc45564a7621141fb34a6273
Arthur Lage <arthurlage2006@gmail.com>
2026-05-24 13:09:55 -0300

    implement new cpu instructions

--- src/cpu.c
+++ src/cpu.c
@@ -31,7 +31,7 @@ void execute(Chip8 *cpu, uint16_t opcode) {
                     // clear display
                     break;
                 case 0x00EE:
-                    cpu->PC = cpu->stack[SP];
+                    cpu->PC = cpu->stack[cpu->SP];
                     cpu->SP -= 1;
                     break;
                 }
@@ -81,13 +81,13 @@ void execute(Chip8 *cpu, uint16_t opcode) {
                     break;
                 case 0x8004:
                     if(cpu->V[x] + cpu->V[y] > 255) {
-                        V[0xF] = 1;
-                        V[x] = cpu->V[x] + cpu->V[y];
+                        cpu->V[0xF] = 1;
+                        cpu->V[x] = cpu->V[x] + cpu->V[y];
                     }
                     break;
                 case 0x8005:
                     if(cpu->V[x] > cpu->V[y]) {
-                        cpu->V[0xF] = 1
+                        cpu->V[0xF] = 1;
                     } else {
                         cpu->V[0xF] = 0;
                     }
@@ -98,7 +98,7 @@ void execute(Chip8 *cpu, uint16_t opcode) {
                     break;
                 case 0x8007:
                     if(cpu->V[y] > cpu->V[x]) {
-                        cpu->V[0xF] = 1
+                        cpu->V[0xF] = 1;
                     } else {
                         cpu->V[0xF] = 0;
                     }
@@ -109,6 +109,11 @@ void execute(Chip8 *cpu, uint16_t opcode) {
                     break;
             }
             break;
+        case 0x9000:
+            if (cpu->V[x] != cpu->V[y]) {
+                cpu->PC += 2;
+            }
+            break;
         case 0xA000:
             cpu->I = nnn;
             break;
@@ -117,6 +122,60 @@ void execute(Chip8 *cpu, uint16_t opcode) {
             break;
         case 0xC000:
             // socorro
+            break;
+        case 0xD000:
+            // TO DO
+            break;
+        case 0xE000:
+            switch(opcode & 0xF0FF) {
+                case 0xE09E:
+                    if(cpu->keypad[cpu->V[x]] == 1) {
+                        cpu->PC += 2;
+                    }
+                    break;
+                case 0xE0A1:
+                    if(cpu->keypad[cpu->V[x]] != 1) {
+                        cpu->PC += 2;
+                    }
+                    break;
+            }
+            break;
+        case 0xF000:
+            switch(opcode & 0xF0FF) {
+                case 0xF007:
+                    cpu->V[x] = cpu->delay_timer;
+                    break;
+                case 0xF00A:
+                    // to do wait for key press
+                    break;
+                case 0xF015:
+                    cpu->delay_timer = cpu->V[x];
+                    break;
+                case 0xF018:
+                    cpu->sound_timer = cpu->V[x];
+                    break;
+                case 0xF01E:
+                    cpu->I += cpu->V[x];
+                    break;
+                case 0xF029:
+                    // to do set vx to digit
+                    break;
+                case 0xF033:
+                    cpu->memory[cpu->I] = cpu->V[x] / 100;
+                    cpu->memory[cpu->I+1] = (cpu->V[x] / 10) % 10;
+                    cpu->memory[cpu->I+2] = cpu->V[x] % 10;
+                    break;
+                case 0xF055:
+                    for (int i = 0; i< x; i++) {
+                        cpu->memory[cpu->I + i] = cpu->V[i];
+                    }
+                    break;
+                case 0xF065:
+                    for(int i = 0; i < x; i++) {
+                        cpu->V[i] = cpu->memory[cpu->I + i];
+                    }
+                    break;
+            }
             break;
         default:
             printf("Opcode desconhecido: %04X\n", opcode);
